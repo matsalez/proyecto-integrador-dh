c0bb2a5642aef11d9d50f0dc78a3ea82c4c9830c
Matias Gonzalez <matsalez@gmail.com>
2019-04-20 16:48:47 -0300

    Agregue index.php, carpeta css y carpetas de imagenes.

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Isabella</title>
+  </head>
+  <body>
+    <!-- inicio header -->
+    <header class="main-header">
+      <nav class="main-nav">
+    		<ul class="sections-home">
+    			<li><a href="#newarrivals">New arrivals</a></li>
+    			<li><a href="#coleccion">Colección</a>
+    				<ul>
+    					<li><a href="#pantalones">Pantalones</a></li>
+    					<li><a href="#remeras">Remeras</a></li>
+    					<li><a href="#faldas">Faldas - Shorts</a></li>
+    					<li><a href="#camperas">Camperas</a></li>
+              <li><a href="#camisas-vestidos">Camisas - Vestidos</a></li>
+    					<li><a href="#buzos-Sweaters">Buzos - Sweaters</a></li>
+    					<li><a href="#jardineros-monos">ardineros - Monos</a></li>
+    					<li><a href="#sale">Sale</a></li>
+    				</ul>
+    			</li>
+    			<li><a href="#accesorios">Accesorios</a></li>
+    			<li><a href="#sprin-summer">Spring/Summer</a>
+          <li><a href="#lookbook">Lookbook</a>
+          <li><a href="#contacto">Contacto</a>
+    			</li>
+    		</ul>
+    	</nav>
+    
+    </header>
+    <!-- Fin header -->
+
+    <!-- Relleno body -->
+    <div class="container">
+    <div class="banner row ">
+      <a href="#coleccion"></a>
+      <img class="banner-coleccion"src="images/banner-coleccion.jpg" alt="banner-coleccion">
+    </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <!-- Fin relleno -->
+
+    <!-- Inicio Footer -->
+    <footer class="main-footer">
+      <ul class="social-media">
+        <li><a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
+        <li><a href="https://www.Twitter.com"><i class="fab fa-twitter-square"></i></a></li>
+        <li><a href="https://www.Instagram.com"><i class="fab fa-instagram"></i></a></li>
+      </ul>
+      <br>
+      <br>
+      <ul class="sections-faq">
+        <li><a href="#como-comprar">¿Cómo comprar?</a></li>
+        <li><a href="#envios">Envíos</a>
+        <li><a href="#devoluciones">Devoluciones</a></li>
+        <li><a href="#talles-locales">Talles y locales</a>
+        <li><a href="#trabaja-con-nosotros">Trabajá con nosotros</a></li>
+        <li><a href="#quienes-somos">Quienés somos </a>
+        <li><a href="#contacto">Contacto</a></li>
+
+    </footer>
+    <!-- Fin footer -->
+
+  </body>
+</html>
