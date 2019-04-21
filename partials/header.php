b0183e1c408ebe0ff5d26c5fa55010e99a230d1c
Matias Gonzalez <matsalez@gmail.com>
2019-04-20 21:13:39 -0300

    Agregue la carpeta partiales, cree header.php dentro de la misma y dejé la estructura del footer en el index .

--- partials/header.php
+++ partials/header.php
@@ -3,12 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>Isabella</title>
   </head>
   <body>
-    <!-- inicio header
+    <!-- inicio header -->
     <header class="main-header">
       <nav class="main-nav">
     		<ul class="sections-home">
@@ -33,14 +33,14 @@
     		</ul>
     	</nav>
 
-    </header> -->
+    </header>
     <!-- Fin header -->
 
     <!-- Relleno body -->
     <div class="container">
     <div class="banner row ">
       <a href="#coleccion"></a>
-      <img class="banner-coleccion"src="images/banner-coleccion.jpg" alt="banner-coleccion">
+      <img class="banner-coleccion"src="../images/banner-coleccion.jpg" alt="banner-coleccion">
     </div>
     </div>
     <br>
@@ -92,27 +92,6 @@
     <br>
     <br>
     <!-- Fin relleno -->
-
-    <!-- Inicio Footer -->
-    <footer class="main-footer">
-      <ul class="social-media">
-        <li><a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
-        <li><a href="https://www.Twitter.com"><i class="fab fa-twitter-square"></i></a></li>
-        <li><a href="https://www.Instagram.com"><i class="fab fa-instagram"></i></a></li>
-      </ul>
-      <br>
-      <br>
-      <ul class="sections-faq">
-        <li><a href="#como-comprar">¿Cómo comprar?</a></li>
-        <li><a href="#envios">Envíos</a>
-        <li><a href="#devoluciones">Devoluciones</a></li>
-        <li><a href="#talles-locales">Talles y locales</a>
-        <li><a href="#trabaja-con-nosotros">Trabajá con nosotros</a></li>
-        <li><a href="#quienes-somos">Quienés somos </a>
-        <li><a href="#contacto">Contacto</a></li>
-
-    </footer>
-    <!-- Fin footer -->
 
   </body>
 </html>
