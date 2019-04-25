6f8ae746426ee0f93e5c7de72a468a7db27590b4
mara270991 <mrv006@hotmail.com>
2019-04-25 17:58:35 -0300

    cree el login

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="css/bootstrap-grid.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,700,900" rel="stylesheet">
+  <title>Login</title>
+</head>
+  <body>
+    <div class="container">
+
+<div class="formulario-login">
+
+    <form class="login" action="index.html" method="post">
+      <img class="logoform"src="images/logo.png" alt="">
+      <br>
+      <br>
+      <label for=""> Usuario: </label>
+      <br>
+      <input type="text" name="user" value="">
+      <br>
+      <label for=""> Email: </label>
+      <br>
+      <input type="text" name="email" value="">
+      <br>
+      <label for=""> Contraseña </label>
+      <br>
+      <input type="password" name="pass" value="">
+      <hr>
+      <br>
+      <button type="submit" class="btn btn-success">Ingresar</button>
+    </form>
+      </div>
+      </div>
+  </body>
+</html>
