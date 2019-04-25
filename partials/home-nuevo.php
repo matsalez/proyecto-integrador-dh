e0423a6d606141a41a40eaafe07d291e9b9a1e64
mara270991 <mrv006@hotmail.com>
2019-04-25 13:58:36 -0300

    modifque el archivo home-nuevo-php

--- partials/home-nuevo.php
+++ partials/home-nuevo.php
@@ -2,9 +2,9 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/home-productsgeneralstyle.css">
+    <link rel="stylesheet" href="../css/home-productsgeneralstyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="../css/bootstrap-grid.css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,700,900" rel="stylesheet">
     <title>Home</title>
   </head>
@@ -12,33 +12,33 @@
     <div class="row">
       <div class="col-12">
         <div class="home-slider">
-          <img src="images/foto-01.jpg" alt="">
+          <img src="../images/foto-01.jpg" alt="">
         </div>
       </div>
     </div>
     <div class="row">
       <div class="col-lg-3 col-md-6 col-xs-12">
         <div class="home-seccion">
-            <img class="home-seccion-img"src="images/foto-05.jpg" alt="newarrivals">
+            <img class="home-seccion-img"src="../images/foto-05.jpg" alt="newarrivals">
             <h2 class="home-title-seccion"> //NEW ARRIVALS </h2>
 
       </div>
       </div>
       <div class="col-lg-3 col-md-6 col-xs-12">
         <div class="home-seccion">
-            <img class="home-seccion-img" src="images/foto-06.jpg" alt="lookbook">
+            <img class="home-seccion-img" src="../images/foto-06.jpg" alt="lookbook">
             <h2 class="home-title-seccion"> #LOOKBOOK </h2>
         </div>
       </div>
       <div class="col-lg-3 col-md-6 col-xs-12">
         <div class="home-seccion">
-            <img class="home-seccion-img" src="images/foto-07.jpg" alt="coleccion">
+            <img class="home-seccion-img" src="../images/foto-07.jpg" alt="coleccion">
             <h2 class="home-title-seccion"> COLECCION </h2>
         </div>
       </div>
       <div class="col-lg-3 col-md-6 col-xs-12">
         <div class="home-seccion">
-          <img class="home-seccion-img" src="images/foto-08.jpg" alt="accesorios">
+          <img class="home-seccion-img" src="../images/foto-08.jpg" alt="accesorios">
           <h2 class="home-title-seccion"> ACCESORIOS </h2>
         </div>
       </div>
@@ -53,17 +53,17 @@
     <div class="row">
       <div class="col-lg-4 col-md-4 col-xs-12">
         <div class="home-socialmedia">
-            <img src="images/foto-02.jpg" alt="">
+            <img src="../images/foto-02.jpg" alt="">
         </div>
       </div>
       <div class="col-lg-4 col-md-4 col-xs-12">
         <div class="home-socialmedia">
-            <img src="images/foto-03.jpg" alt="">
+            <img src="../images/foto-03.jpg" alt="">
         </div>
       </div>
       <div class="col-lg-4 col-md-4 col-xs-12">
         <div class="home-socialmedia">
-            <img src="images/foto-04.jpg" alt="">
+            <img src="../images/foto-04.jpg" alt="">
         </div>
       </div>
     </div>
