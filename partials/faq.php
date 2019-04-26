2bc1bde14f3bb769daffdf2cf9dab432f47bb100
gaztarayn <gaztarayn@gmail.com>
2019-04-26 09:05:54 -0300

    complete un poco mas el faq, aun tengo que terminarlo

--- partials/faq.php
+++ partials/faq.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="css/faqstyle.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>FAQ</title>
+
   </head>
   <body>
     <header>
@@ -13,14 +14,184 @@
     </header>
 
         <div class="faqcuerpo">
-            <h2 class="preguntas"><a href="#como_comprar">¿COMO COMPRAR?</a></h2>
-                <p class="respuestas">
-                      PODES COMPRAR EN NUESTRA PÁGINA “ISABELLA.COM.AR”, HACIENDO CLICK EN LOS DIFERENTES ENLACES UNA VEZ QUE ELIJAS EL PRODUCTO, TE MOSTRARA EL DETALLE DEL PRODUCTO Y TENBDRAS UN BOTON DE “COMPRA” DISPONIBLE PARA COMENZAR LA TRANSACCION.
-                </p>
-        </div>
-  </body>
-</html>
+            <h6 class="preguntas"><a href="#pr1" >¿COMO COMPRAR?</a></h6>
+            <h6 class="preguntas"><a href="#pr2">¿NECESITO TENER CUENTA PARA COMPRAR ONLINE?</a></h6>
+            <h6 class="preguntas"><a href="#pr3">¿OLVIDE MI CONTRASEÑA, COMO LA RECUPERO?</a></h6>
+            <h6 class="preguntas"><a href="#pr6">¿TODO LO QUE PARECE EN LA PAGINA TIENE STOCK?</a></h6>
+            <h6 class="preguntas"><a href="#pr7">¿COMO PUEDO PAGAR MI PEDIDO?</a></h6>
+            <h6 class="preguntas"><a href="#pr8">¿ES SEGURO USAR MI TARJETA DE CREDITO EN EL SITIO?</a></h6>
+            <h6 class="preguntas"><a href="#pr9">¿COMO LOGRO SABER QUE USTEDES RECIBIERON MI ORDEN Y QUE EL PAGO FUE ACREDITADO CORRECTAMENTE?</a></h6>
+            <h6 class="preguntas"><a href="#pr11">¿A QUE ZONAS REALIZAN ENVIOS?</a></h6>
+            <h6 class="preguntas"><a href="#pr12">¿CUANTO CUESTA EL ENVIO?</a></h6>
+            <h6 class="preguntas"><a href="#pr13">¿EN QUE HORARIOS ENTREGA EL CORREO?</a></h6>
+            <h6 class="preguntas"><a href="#pr14">¿PUEDO PROGRAMAR LA ENTREGA DEL ENVIO?</a></h6>
+            <h6 class="preguntas"><a href="#pr15">¿CON QUE PACKAGING LO ENVIAN?</a></h6>
+            <h6 class="preguntas"><a href="#pr16">¿PUEDE RECIBIRLO OTRA PERSONA POR MI?</a></h6>
+            <h6 class="preguntas"><a href="#pr17">¿PUEDO AGREGAR ITEMS UNA VEZ QUE REALICE EL PEDIDO?</a></h6>
+            <h6 class="preguntas"><a href="#pr18">¿A QUE ZONAS REALIZAN ENVIOS?</a></h6>
+            <h6 class="preguntas"><a href="#pr19">¿DONDE ESTA MI PEDIDO?</a></h6>
+            <h6 class="preguntas"><a href="#pr20">¿CUANDO LLEGA MI PEDIDO?</a></h6>
+            <h6 class="preguntas"><a href="#pr21">¿PARA QUE SIRVE EL NUMERO DE GUIA QUE ME ENVIAN?</a></h6>
+            <h6 class="preguntas"><a href="#pr22">¿CUANTAS VECES PASA EL CORREO POR MI DOMICILIO Y QUE SUCEDE SI NO ME ENCUENTRA?</a></h6>
+            <h6 class="preguntas"><a href="#pr23">¿QUIERO REALIZAR UN CAMBIO. COMO HAGO?</a></h6>
+            <h6 class="preguntas"><a href="#pr24">¿QUIERO CANCELAR MI COMPRA, COMO HAGO?</a></h6>
+            <h6 class="preguntas"><a href="#pr25">¿LA MERCADERIA LLEGO FALLADA O DAÑADA. QUE DEBO HACER?</a></h6>
+            <h6 class="preguntas"><a href="#pr26">¿RECIBI UN PRODUCTO DIFERENTE AL QUE COMPRE, CON QUIEN ME CONTACTO?</a></h6>
+            <h6 class="preguntas"><a href="#pr27">¿LOS PRECIOS LISTADOS EN LA PAGINA, SON LOS MISMOS QUE EN LOS LOCALES?</a></h6>
+            <h6 class="preguntas"><a href="#pr28">¿PARA QUE SIRVE EL NUMERO DE GUIA QUE ME ENVIAN?</a></h6>
+            <h6 class="preguntas"><a href="#pr29">¿DONDE SE INGRESA EL CODIGO DE DESCUENTO?</a></h6>
+            <h6 class="preguntas"><a href="#pr30">¿CUAL ES EL HORARIO DE ATENCION DE LA TIENDA?</a></h6>
+            <h6 class="preguntas"><a href="#pr30">¿TENGO MAS PREGUNTAS, COMO LOS CONTACTO?</a></h6>
 
-<!-- <p class="respuestas" id="como_comprar">PARA RECUPERAR TU CONTRASEÑA ENTRA ACA PARA PODER REINICIARLA Y USA UNA NUEVA PARA VOLVER A -->
-<!-- ENTRAR A TU PERFIL </p> -->
-<!-- <a href="#">SUBIR</a> -->
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+
+                  <p class="respuestas" id="pr1">¿COMO COMPRAR? <br><br>
+                    PODES COMPRAR EN NUESTRA PÁGINA “ISABELLA.COM.AR”, HACIENDO CLICK EN LOS DIFERENTES ENLACES UNA VEZ QUE ELIJAS EL PRODUCTO, TE MOSTRARA EL DETALLE DEL PRODUCTO Y TENDRAS UN BOTON DE “COMPRA” DISPONIBLE PARA COMENZAR LA TRANSACCION.</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr2">¿NECESITO TENER CUENTA PARA COMPRAR ONLINE? <br><br>
+                    SI TE RESGISTRAS PODRAS HACER UN SEGUIMIENTO DE TU COMPRAS HASTA QUE LLEGUE A TUS MANOS. ADEMAS DE TENER DIFERENTES PROMOCIONES DISPONIBLES EN TU PERFIL.</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr3">¿OLVIDE MI CONTRASEÑA, COMO LA RECUPERO?<br><br>
+                    PARA RECUPERAR TU CONTRASEÑA ENTRA ACA PARA PODER REINICIARLA Y USA UNA NUEVA PARA VOLVER A ENTRAR A TU PERFIL.</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr6">¿TODO LO QUE PARECE EN LA PAGINA TIENE STOCK?<br><br>
+                    TODOS LOS PRODUCTOS TIENEN STOCK, A MENOS QUE ESTE SEÑALIZADO LO CONTRARIO..</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr7">¿COMO PUEDO PAGAR MI PEDIDO?<br><br>
+                    ACEPTAMOS TODOS LOS MEDIOS DE PAGO INCLUIDOS EN MERCADO PAGO: TARJETAS DE CREDITO Y CUPONES DE PAGO.<br>
+                    LUEGO DE SELECCIONAR TU OPCION PREFERIDA, SERAS REDIRECCIONADO A UN SITIO SEGURO PARA COMPLETAR LOS DATOS NECESARIOS. UNA VEZ TERMINADA LA TRANSACCIÓN RETORNARAS A ISABELLA.COM.AR .</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr28">¿ES SEGURO USAR MI TARJETA DE CREDITO EN EL SITIO?<br><br>
+                    EN ISABELLA NOS TOMAMOS MUY EN SERIO LA SEGURIDAD DE TUS DATOS PERSONALES. TODAS LAS TRANSACCIONES SE REALIZAN A TRAVES DE UN SITIO SEGURO. ADICIONALMENTE, CUANDO RECIBIMOS EL PEDIDO, CORROBORAMOS LA INFORMACION QUE NOS PROPORCIONAS CON LOS DATOS EN ARCHIVO DE LAS TARJETAS DE CREDITO PARA VERIFICAR TU IDENTIDAD Y EVITAR FRAUDES..</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr9">¿DE CUANTO TIEMPO DISPONGO PARA PAGAR UNA VEZ QUE REALICE EL PEDIDO?<br><br>
+                    MERCADOPAGO ES UNO DE LOS PRINCIPALES PROVEEDORES DE SOLUCIONES DE COBRANZAS PARA SITIOS WEB. ESTO PERMITE A INDIVIDUOS Y EMPRESAS PAGAR Y COBRAR DE FORMA CONFIABLE Y SEGURA.</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr9">¿COMO LOGRO SABER QUE USTEDES RECIBIERON MI ORDEN Y QUE EL PAGO FUE ACREDITADO CORRECTAMENTE?<br><br>
+                    RECIBIRAS UN MAIL INMEDIATAMENTE DESPUES DE CONFIRMAR EL PEDIDO Y OTRO MAIL CUANDO EL PAGO SE HAYA ACREDITADO CORRECTAMENTE, INFORMANDOTE QUE EL PEDIDO SE ENCUENTRA “EN PROCESO”..</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr10">¿A QUE ZONAS REALIZAN ENVIOS?<br><br>
+                    REALIZAMOS ENVIOS DENTRO DE TODO EL TERRITORIO DE LA REPUBLICA ARGENTINA..</a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr11">¿CUANTO CUESTA EL ENVIO? <br><br>
+                    EL COSTO DEPENDERA DEL LUGAR DE RESIDENCIA. PARA CAPITAL Y GBA EL COSTO ES DE AR$150 Y PARA EL INTERIOR EL COSTO ES DE AR$ 250.<br>
+                    PARA ENVIO A SUCURSAL OCA EN CAPITAL Y GBA ES DE AR$120 Y PARA SUCURSAL OCA INTERIOR EL COSTO ES DE AR$160.<br>
+                    ENVIO A SUCURSAL ISABELLA NO TIENE COSTO, PERO ENTREGA EN 48 HORAS HABILES.<br>
+                    CUANDO TU PEDIDO ESTE DESPACHADO Y TE LLEGUE LA CONFIRMACION PODRAS PASAR A RETIRAR EL PEDIDO POR EL LIMA 1111 (C.A.B.A.) DE LUNES A VIERNES DE 12 A 18 HORAS CON TU DNI.<br>
+                    EL PEDIDO PERMANECERA POR 5 DÍAS EN LA SUCURSAL, CASO CONTRARIO VOLVERÁ A NUESTRAS OFICINAS .EL CARGO POR REENVÍO A SUCURSAL ES DE $50.<br>
+                    SI TU PEDIDO VA A SER RETIRADO POR OTRA PERSONA, POR FAVOR COMUNICATE CON NOSOTROS AL 0800-333-1111 O POR MAIL AYUDA@ISABELLA.COM.AR PARA PODER AUTORIZARLA.<br>
+                    </a> -->
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr13">¿EN QUE HORARIOS ENTREGA EL CORREO? <br><br>
+PODES COMPRAR EN NUESTRA PÁGINA “ISABELLA.COM.AR”, HACIENDO CLICK EN LOS DIFERENTES ENLACES UNA VEZ QUE ELIJAS EL PRODUCTO, TE MOSTRARA EL DETALLE DEL PRODUCTO Y TENDRAS UN BOTON DE “COMPRA” DISPONIBLE PARA COMENZAR LA TRANSACCION.</a> -->
+                    LAS ENTREGAS DE PEDIDOS A DOMICILIO SE REALIZAN DE LUNES A VIERNES DE 9 A 18HS.  AQUELLOS PEDIDOS A RETIRAR EN SUCURSAL OCA TAMBIÉN LOS SÁBADOS DE 9 A 13 HS. NO SE ENTREGAN PEDIDOS A DOMICILIO NI EN SUCURSAL DOMINGOS NI FERIADOS.
+                    <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr14">¿PUEDO PROGRAMAR LA ENTREGA DEL ENVIO?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr15">¿CON QUE PACKAGING LO ENVIAN?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr16">¿PUEDE RECIBIRLO OTRA PERSONA POR MI? <br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr17"> ¿PUEDO AGREGAR ITEMS UNA VEZ QUE REALICE EL PEDIDO?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr18">¿A QUE ZONAS REALIZAN ENVIOS? <br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr19">¿DONDE ESTA MI PEDIDO?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr20"> ¿CUANDO LLEGA MI PEDIDO?<br><br>
+                 .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr21">¿PARA QUE SIRVE EL NUMERO DE GUIA QUE ME ENVIAN?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr22">¿CUANTAS VECES PASA EL CORREO POR MI DOMICILIO Y QUE SUCEDE SI NO ME ENCUENTRA?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr23"><br>¿QUIERO REALIZAR UN CAMBIO. COMO HAGO?<br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr24">¿QUIERO CANCELAR MI COMPRA, COMO HAGO?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr25">¿LA MERCADERIA LLEGO FALLADA O DAÑADA. QUE DEBO HACER?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr26">¿RECIBI UN PRODUCTO DIFERENTE AL QUE COMPRE, CON QUIEN ME CONTACTO?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr27">¿LOS PRECIOS LISTADOS EN LA PAGINA, SON LOS MISMOS QUE EN LOS LOCALES?<br><br>
+                 .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr28">¿PARA QUE SIRVE EL NUMERO DE GUIA QUE ME ENVIAN?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr29">¿DONDE SE INGRESA EL CODIGO DE DESCUENTO?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr30">¿CUAL ES EL HORARIO DE ATENCION DE LA TIENDA?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+                  <p class="respuestas" id="pr31">¿TENGO MAS PREGUNTAS, COMO LOS CONTACTO?<br><br>
+                  .</a> -->
+                  <a href="#">VOLVER</a>
+                  </p>
+
+                          </div>
+                    </body>
+                  </html>
+
+                  <!-- <p class="respuestas" id="como_comprar">PARA RECUPERAR TU CONTRASEÑA ENTRA ACA PARA PODER REINICIARLA Y USA UNA NUEVA PARA VOLVER A -->
+                  <!-- ENTRAR A TU PERFIL </p> -->
+                  <!-- <a href="#">SUBIR</a> -->
