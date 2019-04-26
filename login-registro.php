b3cb8a0ff70d582ef85d92a5070417dfc72720d1
mara270991 <mrv006@hotmail.com>
2019-04-26 14:19:49 -0300

    agregue el formulario de registro

---
+++ login-registro.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <link rel="stylesheet" href="css/login-registro.css">
+  <link rel="stylesheet" href="css/bootstrap-grid.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,700,900" rel="stylesheet">
+  <title>Login</title>
+</head>
+  <body>
+<div class="fondo">
+    <div class="row">
+        <div class="col-lg-8 col-md-12">
+            <div class="formulario-login">
+                <form class="login" action="index.html" method="post">
+                    <img class="logoform"src="images/logo.png" alt="">
+                    <br>
+                    <br>
+                    <label for=""> Nombre: </label>
+                    <br>
+                    <input type="text" name="user" value="">
+                    <br>
+                    <label for=""> Apellido: </label>
+                    <br>
+                    <input type="text" name="email" value="">
+                    <br>
+                    <hr>
+                    <label for=""> Email: </label>
+                    <br>
+                    <input type="text" name="pass" value="">
+                    <br>
+                    <label for=""> Confirmación de Email </label>
+                    <br>
+                    <input type="text" name="pass" value="">
+                    <br>
+                    <label for=""> Contraseña </label>
+                    <br>
+                    <input type="password" name="pass" value="">
+                    <br>
+                    <label for=""> Confirmacion de Contraseña </label>
+                    <br>
+                    <input type="password" name="pass" value="">
+                    <br>
+                    <br>
+                    <button type="submit" class="btn btn-success">Ingresar</button>
+                    <br>
+                    <br>
+                    <a class="volver "href="index.php"> << Volver al home </a>
+              </form>
+            </div>
+          </div>
+          <div class="col-4">
+              <div class="imagen-registro">
+                  <img src="images/imagen-registro-02.jpg" alt="">
+              </div>
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
