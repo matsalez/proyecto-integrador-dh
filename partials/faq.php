6b72a8f4717dcd851a4e5096f7d43933a8c6772e
gaztarayn <gaztarayn@gmail.com>
2019-04-26 06:55:20 -0300

    Cambie de lugar los archivos FAQ y la extension del archivo

---
+++ partials/faq.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/faqstyle.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>FAQ</title>
+  </head>
+  <body>
+    <header>
+      LOGO Y NOMBRE
+    </header>
+
+        <div class="faqcuerpo">
+            <h2 class="preguntas"><a href="#como_comprar">¿COMO COMPRAR?</a></h2>
+                <p class="respuestas">
+                      PODES COMPRAR EN NUESTRA PÁGINA “ISABELLA.COM.AR”, HACIENDO CLICK EN LOS DIFERENTES ENLACES UNA VEZ QUE ELIJAS EL PRODUCTO, TE MOSTRARA EL DETALLE DEL PRODUCTO Y TENBDRAS UN BOTON DE “COMPRA” DISPONIBLE PARA COMENZAR LA TRANSACCION.
+                </p>
+        </div>
+  </body>
+</html>
+
+<!-- <p class="respuestas" id="como_comprar">PARA RECUPERAR TU CONTRASEÑA ENTRA ACA PARA PODER REINICIARLA Y USA UNA NUEVA PARA VOLVER A -->
+<!-- ENTRAR A TU PERFIL </p> -->
+<!-- <a href="#">SUBIR</a> -->
