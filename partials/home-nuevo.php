37ecb788299c8c8fe37c16ba678475b02e462d3f
mara270991 <mrv006@hotmail.com>
2019-04-26 17:12:06 -0300

    modifique las redes del home-nuevo.php

--- partials/home-nuevo.php
+++ partials/home-nuevo.php
@@ -23,26 +23,32 @@
         <div class="home-seccion">
           <a href="products.php">
             <img class="home-seccion-img"src="images/foto-05.jpg" alt="newarrivals">
-            </a>
+          </a>
             <h2 class="home-title-seccion"> //NEW ARRIVALS </h2>
 
       </div>
       </div>
       <div class="col-lg-3 col-md-6 col-xs-12">
         <div class="home-seccion">
+          <a href="products.php">
             <img class="home-seccion-img" src="images/foto-06.jpg" alt="lookbook">
+            </a>
             <h2 class="home-title-seccion"> #LOOKBOOK </h2>
         </div>
       </div>
       <div class="col-lg-3 col-md-6 col-xs-12">
         <div class="home-seccion">
+          <a href="products.php">
             <img class="home-seccion-img" src="images/foto-07.jpg" alt="coleccion">
+              </a>
             <h2 class="home-title-seccion"> COLECCION </h2>
         </div>
       </div>
       <div class="col-lg-3 col-md-6 col-xs-12">
         <div class="home-seccion">
+          <a href="products.php">
           <img class="home-seccion-img" src="images/foto-08.jpg" alt="accesorios">
+            </a>
           <h2 class="home-title-seccion"> ACCESORIOS </h2>
         </div>
       </div>
@@ -57,17 +63,27 @@
     <div class="row">
       <div class="col-lg-4 col-md-4 col-xs-12">
         <div class="home-socialmedia">
+          <a href="products.php">
             <img src="images/foto-02.jpg" alt="">
+            </a>
+            <img class="social-icon" src="images/social-10.png" alt="">
         </div>
       </div>
       <div class="col-lg-4 col-md-4 col-xs-12">
+
         <div class="home-socialmedia">
+      <a href="products.php">
             <img src="images/foto-03.jpg" alt="">
+            </a>
+              <img class="social-icon" src="images/social-11.png" alt="">
         </div>
       </div>
       <div class="col-lg-4 col-md-4 col-xs-12">
         <div class="home-socialmedia">
+          <a href="products.php">
             <img src="images/foto-04.jpg" alt="">
+            </a>
+              <img class="social-icon" src="images/social-09.png" alt="">
         </div>
       </div>
     </div>
