daf51acfb2edead7dd365a7888625fbb16cbbe33
gaztarayn <gaztarayn@gmail.com>
2019-04-26 11:30:39 -0300

    Modifique el faq index, ahora si linkea el faqstyle

--- partials/faq.php
+++ partials/faq.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/faqstyle.css">
+    <link rel="stylesheet" href="../css/faqstyle.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>FAQ</title>
 
@@ -14,12 +14,12 @@
     </header>
 
         <div class="faqcuerpo">
-            <h6 class="preguntas"><a href="#pr1" >¿COMO COMPRAR?</a></h6>
-            <h6 class="preguntas"><a href="#pr2">¿NECESITO TENER CUENTA PARA COMPRAR ONLINE?</a></h6>
-            <h6 class="preguntas"><a href="#pr3">¿OLVIDE MI CONTRASEÑA, COMO LA RECUPERO?</a></h6>
-            <h6 class="preguntas"><a href="#pr6">¿TODO LO QUE PARECE EN LA PAGINA TIENE STOCK?</a></h6>
-            <h6 class="preguntas"><a href="#pr7">¿COMO PUEDO PAGAR MI PEDIDO?</a></h6>
-            <h6 class="preguntas"><a href="#pr8">¿ES SEGURO USAR MI TARJETA DE CREDITO EN EL SITIO?</a></h6>
+            <h4 class="preguntas"><a href="#pr1" >¿COMO COMPRAR?</a></h4>
+            <h4 class="preguntas"><a href="#pr2">¿NECESITO TENER CUENTA PARA COMPRAR ONLINE?</a></h4>
+            <h4 class="preguntas"><a href="#pr3">¿OLVIDE MI CONTRASEÑA, COMO LA RECUPERO?</a></h4>
+            <h4 class="preguntas"><a href="#pr6">¿TODO LO QUE PARECE EN LA PAGINA TIENE STOCK?</a></h4>
+            <h4 class="preguntas"><a href="#pr7">¿COMO PUEDO PAGAR MI PEDIDO?</a></h4>
+            <h4 class="preguntas"><a href="#pr8">¿ES SEGURO USAR MI TARJETA DE CREDITO EN EL SITIO?</a></h4>
             <h6 class="preguntas"><a href="#pr9">¿COMO LOGRO SABER QUE USTEDES RECIBIERON MI ORDEN Y QUE EL PAGO FUE ACREDITADO CORRECTAMENTE?</a></h6>
             <h6 class="preguntas"><a href="#pr11">¿A QUE ZONAS REALIZAN ENVIOS?</a></h6>
             <h6 class="preguntas"><a href="#pr12">¿CUANTO CUESTA EL ENVIO?</a></h6>
@@ -41,7 +41,7 @@
             <h6 class="preguntas"><a href="#pr28">¿PARA QUE SIRVE EL NUMERO DE GUIA QUE ME ENVIAN?</a></h6>
             <h6 class="preguntas"><a href="#pr29">¿DONDE SE INGRESA EL CODIGO DE DESCUENTO?</a></h6>
             <h6 class="preguntas"><a href="#pr30">¿CUAL ES EL HORARIO DE ATENCION DE LA TIENDA?</a></h6>
-            <h6 class="preguntas"><a href="#pr30">¿TENGO MAS PREGUNTAS, COMO LOS CONTACTO?</a></h6>
+            <h6 class="preguntas"><a href="#pr31">¿TENGO MAS PREGUNTAS, COMO LOS CONTACTO?</a></h6>
 
             <br>
             <br>
@@ -84,7 +84,7 @@
                     LUEGO DE SELECCIONAR TU OPCION PREFERIDA, SERAS REDIRECCIONADO A UN SITIO SEGURO PARA COMPLETAR LOS DATOS NECESARIOS. UNA VEZ TERMINADA LA TRANSACCIÓN RETORNARAS A ISABELLA.COM.AR .</a> -->
                     <a href="#">VOLVER</a>
                   </p>
-                  <p class="respuestas" id="pr28">¿ES SEGURO USAR MI TARJETA DE CREDITO EN EL SITIO?<br><br>
+                  <p class="respuestas" id="pr8">¿ES SEGURO USAR MI TARJETA DE CREDITO EN EL SITIO?<br><br>
                     EN ISABELLA NOS TOMAMOS MUY EN SERIO LA SEGURIDAD DE TUS DATOS PERSONALES. TODAS LAS TRANSACCIONES SE REALIZAN A TRAVES DE UN SITIO SEGURO. ADICIONALMENTE, CUANDO RECIBIMOS EL PEDIDO, CORROBORAMOS LA INFORMACION QUE NOS PROPORCIONAS CON LOS DATOS EN ARCHIVO DE LAS TARJETAS DE CREDITO PARA VERIFICAR TU IDENTIDAD Y EVITAR FRAUDES..</a> -->
                     <a href="#">VOLVER</a>
                   </p>
@@ -92,15 +92,15 @@
                     MERCADOPAGO ES UNO DE LOS PRINCIPALES PROVEEDORES DE SOLUCIONES DE COBRANZAS PARA SITIOS WEB. ESTO PERMITE A INDIVIDUOS Y EMPRESAS PAGAR Y COBRAR DE FORMA CONFIABLE Y SEGURA.</a> -->
                     <a href="#">VOLVER</a>
                   </p>
-                  <p class="respuestas" id="pr9">¿COMO LOGRO SABER QUE USTEDES RECIBIERON MI ORDEN Y QUE EL PAGO FUE ACREDITADO CORRECTAMENTE?<br><br>
+                  <p class="respuestas" id="pr10">¿COMO LOGRO SABER QUE USTEDES RECIBIERON MI ORDEN Y QUE EL PAGO FUE ACREDITADO CORRECTAMENTE?<br><br>
                     RECIBIRAS UN MAIL INMEDIATAMENTE DESPUES DE CONFIRMAR EL PEDIDO Y OTRO MAIL CUANDO EL PAGO SE HAYA ACREDITADO CORRECTAMENTE, INFORMANDOTE QUE EL PEDIDO SE ENCUENTRA “EN PROCESO”..</a> -->
                     <a href="#">VOLVER</a>
                   </p>
-                  <p class="respuestas" id="pr10">¿A QUE ZONAS REALIZAN ENVIOS?<br><br>
+                  <p class="respuestas" id="pr11">¿A QUE ZONAS REALIZAN ENVIOS?<br><br>
                     REALIZAMOS ENVIOS DENTRO DE TODO EL TERRITORIO DE LA REPUBLICA ARGENTINA..</a> -->
                     <a href="#">VOLVER</a>
                   </p>
-                  <p class="respuestas" id="pr11">¿CUANTO CUESTA EL ENVIO? <br><br>
+                  <p class="respuestas" id="pr12">¿CUANTO CUESTA EL ENVIO? <br><br>
                     EL COSTO DEPENDERA DEL LUGAR DE RESIDENCIA. PARA CAPITAL Y GBA EL COSTO ES DE AR$150 Y PARA EL INTERIOR EL COSTO ES DE AR$ 250.<br>
                     PARA ENVIO A SUCURSAL OCA EN CAPITAL Y GBA ES DE AR$120 Y PARA SUCURSAL OCA INTERIOR EL COSTO ES DE AR$160.<br>
                     ENVIO A SUCURSAL ISABELLA NO TIENE COSTO, PERO ENTREGA EN 48 HORAS HABILES.<br>
@@ -111,7 +111,7 @@
                     <a href="#">VOLVER</a>
                   </p>
                   <p class="respuestas" id="pr13">¿EN QUE HORARIOS ENTREGA EL CORREO? <br><br>
-PODES COMPRAR EN NUESTRA PÁGINA “ISABELLA.COM.AR”, HACIENDO CLICK EN LOS DIFERENTES ENLACES UNA VEZ QUE ELIJAS EL PRODUCTO, TE MOSTRARA EL DETALLE DEL PRODUCTO Y TENDRAS UN BOTON DE “COMPRA” DISPONIBLE PARA COMENZAR LA TRANSACCION.</a> -->
+                    PODES COMPRAR EN NUESTRA PÁGINA “ISABELLA.COM.AR”, HACIENDO CLICK EN LOS DIFERENTES ENLACES UNA VEZ QUE ELIJAS EL PRODUCTO, TE MOSTRARA EL DETALLE DEL PRODUCTO Y TENDRAS UN BOTON DE “COMPRA” DISPONIBLE PARA COMENZAR LA TRANSACCION.</a> -->
                     LAS ENTREGAS DE PEDIDOS A DOMICILIO SE REALIZAN DE LUNES A VIERNES DE 9 A 18HS.  AQUELLOS PEDIDOS A RETIRAR EN SUCURSAL OCA TAMBIÉN LOS SÁBADOS DE 9 A 13 HS. NO SE ENTREGAN PEDIDOS A DOMICILIO NI EN SUCURSAL DOMINGOS NI FERIADOS.
                     <a href="#">VOLVER</a>
                   </p>
