b04392eb46c956d3dc1855ef6472e46ecc705ae6
mara270991 <mrv006@hotmail.com>
2019-04-26 23:45:19 -0300

    mejore el footer en movil

--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,6 @@
 <footer class="main-footer">
+  <br class="brfooter">
+  <hr class="hrfooter">
   <ul class="social-media">
     <li><a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
     <li><a href="https://www.Twitter.com"><i class="fab fa-twitter-square"></i></a></li>
