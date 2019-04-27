df731a72731e466cc57eed92c03d83ec5c3062e4
mara270991 <mrv006@hotmail.com>
2019-04-26 23:23:28 -0300

    separe el css del footer, ya que no toma algunos estilos- modifique pequeñas cosas del productos y lo pase a partials

--- products.php
+++ products.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/navstyle.css">
+    <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> <!-- CDN Jquery-->
     <link rel="stylesheet" href="css/home-productsgeneralstyle.css">
